4ac79f2c6af2dc59b73b9fd76cf25626e9745a72
Umar <apple@Umars-MacBook-Pro.local>
2022-11-03 15:18:06 +0500

    debug on

--- Server/settings.py
+++ Server/settings.py
@@ -91,10 +91,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-if not DEBUG:
-    import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=600)
-    DATABASES['default'].update(db_from_env)
+# if not DEBUG:
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
