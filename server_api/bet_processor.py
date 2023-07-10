ba518304b9777f24f9ee3cd4e63c343decb2b525
umarz317 <umar.317@gmail.com>
2023-07-11 00:49:36 +0500

    Update bet_processor.py

--- server_api/bet_processor.py
+++ server_api/bet_processor.py
@@ -1,14 +1,34 @@
+import random
+
 import eth_account.messages
 from web3 import Web3
 import os
-from web3.auto import w3
 
 # private key
 key = os.getenv('PK')
-print(key)
 
 
-def processBet(receiver, amount):
+def init_web3():
+    return Web3(Web3.HTTPProvider('https://matic-mumbai.chainstacklabs.com'))
+
+
+def check_tx(hash):
+    w3 = init_web3()
+    tx = w3.eth.get_transaction(hash)
+    print(Web3.toInt(tx.value))
+
+def simulate_game(amount,multiplier):
+    payout = multiplier*amount
+    cutoff = multiplier * (1/payout)
+    random_number = random.randrange(0, multiplier*2)
+    print(random_number,cutoff)
+    if random_number<cutoff:
+        return 'won'
+    else:
+        return 'busted'
+
+def processBet(tx_hash, receiver, amount):
+    w3 = init_web3()
     receiver = Web3.toChecksumAddress(receiver)
     messageHash = Web3.solidityKeccak(['address', 'uint256'], [receiver, amount])
     signableMessage = eth_account.messages.encode_defunct(messageHash)
@@ -22,3 +42,7 @@ def processBet(receiver, amount):
 
 def to_32byte_hex(val):
     return Web3.toHex(Web3.toBytes(val).rjust(32, b'\0'))
+
+
+# check_tx('0xcd6d5f0157295cba3214485f32597b62842d138fd2b800a31921832161ed180e')
+print(simulate_game(1,5))
